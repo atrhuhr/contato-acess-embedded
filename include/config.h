9b60dcf6a930a48db4f2659bf4b76b44e2f45e57
Arthur <aranquine@gmail.com>
2026-06-02 17:58:03 -0300

    02-06

--- include/config.h
+++ include/config.h
@@ -23,7 +23,7 @@ static const char *FILE_DIR        = "/dir.bin";
 static const char *FILE_IMU_OFFSETS= "/imu_offsets.bin";
 
 // ─── Calibração IMU ───────────────────────────────────────────────────────────
-static const unsigned long CALIB_DURATION_MS = 2000;
+static const unsigned long CALIB_DURATION_MS = 3000;
 
 // ─── Temporização ────────────────────────────────────────────────────────────
 static const unsigned long STATUS_INTERVAL_MS = 3;
